0124f9c239406eb419c9c8efbbcf9bfff8dab09d
ImTreisis <imtreisis@gmail.com>
2025-10-18 15:52:44 +0100

    Update
    Update

--- backend/index.js
+++ backend/index.js
@@ -96,7 +96,6 @@ mongoose
     maxPoolSize: 10, // Maximum number of connections in the connection pool
     serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    bufferMaxEntries: 0, // Disable mongoose buffering
     bufferCommands: false, // Disable mongoose buffering
   })
   .then(() => {
